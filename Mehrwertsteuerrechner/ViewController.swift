cbe5db69b8392c06517929b96d24523a9eff0e7d
Meyblaubaer <scmapps@icloud.com>
2022-03-19 15:44:51 +0100

    Initial Commit

---
+++ Mehrwertsteuerrechner/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Mehrwertsteuerrechner
+//
+//  Created by Sven-Christian Meyhoefer on 06.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var kaufBetrag: UITextField!
+    
+
+    @IBOutlet weak var mwstSatz: UITextField!
+    
+    @IBOutlet weak var netto: UILabel!
+    
+    @IBOutlet weak var steuer: UILabel!
+    
+    @IBOutlet weak var brutto: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func berechnen(_ sender: UIButton) {
+        let betrag = Int(kaufBetrag.text!) ?? 0
+        let steuerSatz = Int(mwstSatz.text!) ?? 0
+        
+        var steuerBerechnung = betrag * steuerSatz / 100
+        
+        steuer.text = String (steuerBerechnung)
+        
+        netto.text = "\(betrag)"
+        
+        brutto.text = String(steuerBerechnung + betrag)
+        
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+    }
+    
+    }
